ed3988d5eef37a3dae517b8622e332a256a6da96
Toriality <pcrelier@hotmail.com>
2022-04-19 21:05:03 -0300

    Add custom article background image with php

--- body.php
+++ body.php
@@ -3,11 +3,20 @@
   $charset = $mysqli->set_charset('utf8');
 
   $id = $_GET['id'];
+
+  $background_with_template = "./images/banner.png";
+  $background_with_id = "./images/" . $id . ".jpg";
+
+  if (file_exists($background_with_id)) {
+    $background = $background_with_id;
+  } else {
+    $background = $background_with_template;
+  }
   $result = $mysqli->query("SELECT * FROM articles WHERE id=" . $id);
   $row = $result->fetch_assoc()
 ?>
 <article>
-  <div id="background">
+  <div id="background" style="background-image: url('<?=$background?>')" >
     <div id="title">
       <h1>The DYOM Journal #<?= $row['ID'] ?></h1>
       <h2><?= $row['Title'] ?></h2>
