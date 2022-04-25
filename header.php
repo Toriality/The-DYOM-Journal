72a87dfbbac678b8c01eefa5386420f824e62dd7
Toriality <pcrelier@hotmail.com>
2022-04-25 06:23:57 -0300

    Few style adjusts

--- header.php
+++ header.php
@@ -9,20 +9,9 @@
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">The DYOM Journal</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <nav>
+        <div class="navbar">
+          <a class="navbar-title" href="#">The DYOM Journal</a>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <form class="d-flex">
               <input
